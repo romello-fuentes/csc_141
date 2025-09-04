78ef5bb98d21e6185ced05976aab30fb47900a83
romello_fuentes <romfue123@gmail.com>
2025-09-04 11:47:26 -0400

    Add files via upload

--- assignments/01_getting_started/01_01_python_org.py
+++ assignments/01_getting_started/01_01_python_org.py
@@ -1,8 +1,8 @@
 # pg 51 try it yourself
 
 '''
-I am looking through python.org 
+I am looking through python.org.
 I am intrested in business, scientific,
- and software development coding
+and software development coding
 
 '''
