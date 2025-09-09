7c9e2b5f2d53f536cb9f982e8fb080fbabf71a7c
romello_fuentes <romfue123@gmail.com>
2025-09-09 08:15:28 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_05_famous_quote.py
@@ -0,0 +1,2 @@
+# 2-5. Famous Quote
+print('Bruce Lee once said, “Be like water making its way through cracks. Do not be assertive, but adjust to the object, and you shall find a way around or through it. If nothing within you stays rigid, outward things will disclose themselves. Empty your mind, be formless. Shapeless, like water. If you put water into a cup, it becomes the cup. You put water into a bottle and it becomes the bottle. You put it in a teapot, it becomes the teapot. Now, water can flow or it can crash. Be water, my friend.”')
