d9b9aa33d7a66851185afe7f2efeaebe7bcb1232
romello_fuentes <romfue123@gmail.com>
2025-09-09 08:26:20 -0400

    Add files via upload

---
+++ assignments/02_variables_and_data_types/02_07_stripping_names.py
@@ -0,0 +1,14 @@
+# 2-7. Stripping Names
+name = "\t\n  Eric  \n\t"
+
+print("Original with whitespace:")
+print(name)
+
+print("\nUsing lstrip():")
+print(name.lstrip())
+
+print("\nUsing rstrip():")
+print(name.rstrip())
+
+print("\nUsing strip():")
+print(name.strip())
