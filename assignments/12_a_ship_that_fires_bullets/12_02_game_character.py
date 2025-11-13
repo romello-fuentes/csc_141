67f33512b3d42986b718e5ab27e5f2a9c75e3cfd
romello_fuentes <romfue123@gmail.com>
2025-11-13 11:10:09 -0500

    Add files via upload

---
+++ assignments/12_a_ship_that_fires_bullets/12_02_game_character.py
@@ -0,0 +1,34 @@
+# 12-2, game character, 276
+
+#Art is made in chatgpt
+
+import pygame
+import sys
+
+class GameCharacter:
+    def __init__(self, screen):
+        self.screen = screen
+        self.image = pygame.image.load("C:/Users/romfu/Desktop/csc_141/assignments/12_a_ship_that_fires_bullets/ship.bmp")
+        self.rect = self.image.get_rect()
+        self.rect.center = screen.get_rect().center
+
+    def draw(self):
+        self.screen.blit(self.image, self.rect)
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Game Character")
+
+bg_color = (135, 206, 235)  
+
+character = GameCharacter(screen)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill(bg_color)
+    character.draw()
+    pygame.display.flip()
